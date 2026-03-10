04650ee9904c79ce1c0d6e15bf48d8b11e93e0e2
Vedran Rudelj <vedran@unchained.shop>
2026-03-10 15:17:43 +0100

    Switch to Python HTTP server for Railway deployment

---
+++ server.py
@@ -0,0 +1,9 @@
+import http.server
+import os
+
+port = int(os.environ.get("PORT", 8001))
+
+handler = http.server.SimpleHTTPRequestHandler
+httpd = http.server.HTTPServer(("0.0.0.0", port), handler)
+print(f"Serving on :{port}")
+httpd.serve_forever()
